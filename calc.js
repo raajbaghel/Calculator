1f276b134066cc5be606daaf9a9f23c1a4408431
Raj Baghel <127883148+raajbaghel@users.noreply.github.com>
2023-06-16 19:27:00 +0530

    calc.js uploaded

---
+++ calc.js
@@ -0,0 +1,23 @@
+const button = document.getElementsByClassName('button');
+const inputField = document.getElementById('number-input');
+
+for (let i = 0; i < button.length; i++) {
+ button[i].addEventListener('click', (event) => {
+
+  let inputText = '';
+  switch (event.target.innerHTML) {
+    case 'C':
+         break;
+    case '=':
+       inputText = eval(inputField.value);
+         break;
+    case '←':
+       inputText = inputField.value.slice(0,inputField.value.length-1);
+         break;
+    default:
+        inputText =  `${inputField.value}${event.target.innerHTML}`;
+  }
+
+
+    inputField.value = inputText;
+ }) }
